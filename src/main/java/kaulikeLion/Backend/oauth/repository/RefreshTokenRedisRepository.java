88e128d8b2e3cd94a137dbaafb341a98fe6fdc2e
persi0815 <jina_8151010@naver.com>
2024-02-05 00:20:41 +0900

    Feature: Refresh token 관련 로직

---
+++ src/main/java/kaulikeLion/Backend/oauth/repository/RefreshTokenRedisRepository.java
@@ -0,0 +1,12 @@
+package kaulikeLion.Backend.oauth.repository;
+
+import kaulikeLion.Backend.oauth.jwt.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken); //reissue
+    boolean existsById(String username);
+    void deleteById(String username);
+}
