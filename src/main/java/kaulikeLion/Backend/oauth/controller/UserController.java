ae4b2a1d426dd66dcc687c7d841587ed9a92a366
persi0815 <jina_8151010@naver.com>
2024-02-14 21:46:34 +0900

    Refactor: /reissue 추가

--- src/main/java/kaulikeLion/Backend/oauth/controller/UserController.java
+++ src/main/java/kaulikeLion/Backend/oauth/controller/UserController.java
@@ -3,7 +3,7 @@ package kaulikeLion.Backend.oauth.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import kaulikeLion.Backend.global.api_payload.ApiResponse;
 import kaulikeLion.Backend.global.api_payload.SuccessCode;
-import kaulikeLion.Backend.oauth.jwt.JwtTokenUtils;
+import kaulikeLion.Backend.oauth.jwt.JwtDto;
 import kaulikeLion.Backend.oauth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -14,30 +14,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
-    private final JwtTokenUtils jwtTokenUtils;
 
-    // 로그아웃
     @DeleteMapping("/logout")
     public ApiResponse<Integer> logout(HttpServletRequest request) {
         userService.logout(request);
         return ApiResponse.onSuccess(SuccessCode.USER_LOGOUT_SUCCESS, 1);
     }
 
-    // 회원탈퇴
+    @PostMapping("/reissue")
+    public ApiResponse<JwtDto> reissue(
+            HttpServletRequest request
+    ) {
+        JwtDto jwt = userService.reissue(request);
+        return ApiResponse.onSuccess(SuccessCode.USER_REISSUE_SUCCESS, jwt);
+    }
+
+
     @DeleteMapping("/me")
     public ApiResponse<Integer> deleteUser(Authentication auth) {
         userService.deleteUser(auth.getName());
         return ApiResponse.onSuccess(SuccessCode.USER_DELETE_SUCCESS, 1);
     }
-/*
-    // 토큰 재발급
-    @PostMapping("/reissue")
-    public ApiResponse<UserResponseDto.ReissueDto> reissue(
-            HttpServletRequest request
-    ) {
-        JwtDto jwt = userService.reissue(request);
-        return ApiResponse.onSuccess(UserConverter.toLogoutDto(jwt));
-    }
-*/
-
 }
