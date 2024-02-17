2097f0dd8fe383da4f13fa21fbf0b78bc8fbf6d5
persi0815 <jina_8151010@naver.com>
2024-02-17 16:47:37 +0900

    Feature: 과제 관련 SuccessCode

--- src/main/java/kaulikeLion/Backend/global/api_payload/SuccessCode.java
+++ src/main/java/kaulikeLion/Backend/global/api_payload/SuccessCode.java
@@ -16,9 +16,21 @@ public enum SuccessCode implements BaseCode {
     USER_LOGOUT_SUCCESS(HttpStatus.OK, "USER_202", "로그아웃 되었습니다."),
     USER_REISSUE_SUCCESS(HttpStatus.OK, "USER_203", "토큰 재발급이 완료되었습니다."),
     USER_DELETE_SUCCESS(HttpStatus.OK, "USER_204", "회원탈퇴가 완료되었습니다."),
+
     //USER_PASSWORD_CHANGE_SUCCESS(HttpStatus.OK, "USER_205", "비밀번호가 변경되었습니다."),
 
-    // FILE
+    // Assignment
+    ASSIGNMENT_CREATED(HttpStatus.CREATED, "ASSIGNMENT_201", "과제 생성이 완료되었습니다."),
+    ASSIGNMENT_LIST_VIEW_SUCCESS(HttpStatus.OK, "ASSIGNMENT_202", "과제 목록 조회가 완료되었습니다."),
+    ASSIGNMENT_DETAIL_VIEW_SUCCESS(HttpStatus.OK, "ASSIGNMENT_203", "과제 상세 조회가 완료되었습니다."),
+    ASSIGNMENT_UPDATED(HttpStatus.OK, "ASSIGNMENT_204", "글 수정이 완료되었습니다."),
+    ASSIGNMENT_DELETED(HttpStatus.OK, "ASSIGNMENT_205", "과제 삭제가 완료되었습니다."),
+
+    // Submission
+    SUBMISSION_CREATED(HttpStatus.CREATED, "SUBMISSION_201", "글 생성이 완료되었습니다."),
+    SUBMISSION_DELETED(HttpStatus.OK, "SUBMISSION_202", "글 삭제가 완료되었습니다."),
+
+    // File
     FILE_UPLOAD_SUCCESS(HttpStatus.OK, "FILE_200", "파일 업로드 완료되었습니다."),
     FILE_DELETE_SUCCESS(HttpStatus.OK, "FILE_201", "파일 삭제 완료되었습니다."),
     ;
