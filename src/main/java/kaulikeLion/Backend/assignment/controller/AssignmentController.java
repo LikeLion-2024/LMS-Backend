7c545a4b68bd3626d56c079f610726d39cd58d46
persi0815 <jina_8151010@naver.com>
2024-02-17 16:51:03 +0900

    Refactor: Assignment create, list, detail, update, delete controller

--- src/main/java/kaulikeLion/Backend/assignment/controller/AssignmentController.java
+++ src/main/java/kaulikeLion/Backend/assignment/controller/AssignmentController.java
@@ -1,97 +1,95 @@
 package kaulikeLion.Backend.assignment.controller;
 
+import kaulikeLion.Backend.assignment.converter.AssignmentConverter;
 import kaulikeLion.Backend.assignment.domain.Assignment;
-import kaulikeLion.Backend.assignment.dto.AssignmentDto;
+import kaulikeLion.Backend.assignment.domain.Submission;
+import kaulikeLion.Backend.global.api_payload.SuccessCode;
 import kaulikeLion.Backend.assignment.service.AssignmentService;
 import kaulikeLion.Backend.assignment.service.SubmissionService;
+import kaulikeLion.Backend.global.api_payload.ApiResponse;
+import kaulikeLion.Backend.oauth.domain.User;
+import kaulikeLion.Backend.oauth.jwt.CustomUserDetails;
+import kaulikeLion.Backend.oauth.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 import java.util.List;
 
-@Controller
+import static kaulikeLion.Backend.assignment.dto.AssignmentRequestDto.*;
+import static kaulikeLion.Backend.assignment.dto.AssignmentResponseDto.*;
+
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/assignment")
 public class AssignmentController {
 
+    private final UserService userService;
     private final AssignmentService assignmentService;
-//    private final SubmissionService submissionService;
+    private final SubmissionService submissionService;
 
-    // 과제 만들기 ing - 사진 (+파일..?)
-    @GetMapping("/save")
-    public String saveForm() {
-        return "save";
+    // 과제 만들기
+    @PostMapping("/create")
+    public ApiResponse<SimpleAssignmentDto> create(
+            @RequestBody AssignmentReqDto assignmentReqDto,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        User user = userService.findUserByUserName(customUserDetails.getUsername());
+        Assignment assignment = assignmentService.createAssignment(assignmentReqDto, user);
+
+        return ApiResponse.onSuccess(SuccessCode.ASSIGNMENT_CREATED, AssignmentConverter.simpleAssignmentDto(assignment));
     }
 
-    // 과제 만들기 end
-    @PostMapping("/save")
-    public String save(@ModelAttribute AssignmentDto assignmentDto) throws IOException {
-        System.out.println("assignmentDto = " + assignmentDto);
-        assignmentService.save(assignmentDto);
-        return "index"; // 과제 목록 나옴
+    // 과제 목록 조회 - 누구나 접근 가능
+    @GetMapping("/list")
+    public ApiResponse<AssignmentListResDto> list(
+            @RequestParam(name = "page") Integer page
+    ){
+        List<Assignment> assignmentsByUser = assignmentService.findAll(); // 모든 과제가 모든 인원에게 할당됨
+        Page<Assignment> assignments = assignmentService.convertAssignmentToPage(assignmentsByUser, page, 5);
+
+        return ApiResponse.onSuccess(SuccessCode.ASSIGNMENT_LIST_VIEW_SUCCESS, AssignmentConverter.assignmentListResDto(assignments));
     }
 
-    // 과제 목록 조회 - 홈 화면
-    @GetMapping("/")
-    public String findAll(Model model){ //데이터를 가져올때 model 객체 사용
-        // DB에서 전체 게시글 데이터를 가져와서 index.html에 보여준다
-        List<AssignmentDto> assignmentDtoList = assignmentService.findAll();
-        model.addAttribute("assignmentList", assignmentDtoList);
-        return "list";
-    }
-    // 페이징
-//    @GetMapping("/paging") // 기본적으로 1페이지 보여줌
-//    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model){
-//        Page<AssignmentDto> boardList = boardService.paging(pageable);
-//
-//        int blockLimit = 3; // 보여지는 페이지 번호 개수
-//        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
-//        int endPage = Math.min((startPage + blockLimit - 1), boardList.getTotalPages()); // 3 6 9 ~~
-//
-//        model.addAttribute("boardList", boardList);
-//        model.addAttribute("startPage", startPage);
-//        model.addAttribute("endPage", endPage);
-//        return "paging";
-//    }
-
-    // 과제 상세 조회 - 과제 제출란 있어야
+    // 과제 상세 조회
     @GetMapping("/{id}")
-    public String findById(@PathVariable(name = "id") Long id, Model model) { // 경로상에 있는 값 가져올땐 PathVariable사용
-        // 해당 게시글의 조회수를 하나 올리기
-        assignmentService.updateHits(id);
-        // 게시글 데이터를 가져와서 detail.html에 출력
-        AssignmentDto assignmentDto = assignmentService.findById(id);
+    public ApiResponse<DetailAssignmentDto> detail(
+            @PathVariable(name = "id") Long id,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        // 해당 게시글의 조회수 하나 올리기
+        Assignment assignment = assignmentService.increaseViewCount(assignmentService.findById(id));
+
         // 과제 제출 목록 가져오기
-//        List<SubmissionDto> submissionDtoList = submissionService.findAll(id);
-//        model.addAttribute("submissionList", submissionDtoList);
-        model.addAttribute("assignment", assignmentDto);
+        List<Submission> submissions = submissionService.findAllByAssignmentId(id);
 
-        return "detail";
+        // 과제 파일 제출 목록 가져오기
+
+        return ApiResponse.onSuccess(SuccessCode.ASSIGNMENT_DETAIL_VIEW_SUCCESS, AssignmentConverter.detailAssignmentDto(assignment, submissions));
     }
 
-    // 과제 수정 ing
-    @GetMapping("/update/{id}")
-    public String updateForm(@PathVariable(name = "id") Long id, Model model){
-        AssignmentDto assignmentDto = assignmentService.findById(id);
-        model.addAttribute("assignmentUpdate", assignmentDto);
-        return "update";
-    }
+    // 과제 수정
+    @PostMapping("/update/{id}")
+    public ApiResponse<DetailAssignmentDto> update(
+            @PathVariable(name = "id") Long id,
+            @RequestBody DetailAssignmentReqDto detailAssignmentReqDto,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        Assignment assignment = assignmentService.updateAssignment(id, detailAssignmentReqDto);
+        List<Submission> submissions = submissionService.findAllByAssignmentId(id);
 
-    // 과제 수정 end
-    @PostMapping("/update")
-    public String update(@ModelAttribute AssignmentDto assignmentDto, Model model){
-        AssignmentDto assignment = assignmentService.update(assignmentDto);
-        model.addAttribute("assignment", assignment);
-        return "detail"; //수정 반영된 객체 가지고 화면에 띄움
+        return ApiResponse.onSuccess(SuccessCode.ASSIGNMENT_UPDATED, AssignmentConverter.detailAssignmentDto(assignment, submissions));
     }
 
     // 과제 삭제
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable(name = "id") Long id){
-        assignmentService.delete(id);
-        return "redirect:/assignment/"; // 띄어쓰기 하면 안됨
+    @DeleteMapping("/delete/{id}")
+    public ApiResponse<Integer> delete(
+            @PathVariable(name = "id") Long id,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ){
+        assignmentService.deleteAssignment(id);
+
+        return ApiResponse.onSuccess(SuccessCode.ASSIGNMENT_DELETED, 1);
     }
 }
