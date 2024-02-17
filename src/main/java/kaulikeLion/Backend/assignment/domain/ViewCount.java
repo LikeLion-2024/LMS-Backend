855aaccb484a55e4c9205989424a7f57887dc021
persi0815 <jina_8151010@naver.com>
2024-02-17 16:47:58 +0900

    Feature: ViewCount entity

---
+++ src/main/java/kaulikeLion/Backend/assignment/domain/ViewCount.java
@@ -0,0 +1,22 @@
+package kaulikeLion.Backend.assignment.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViewCount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignment_id")
+    private Assignment assignment;
+}
