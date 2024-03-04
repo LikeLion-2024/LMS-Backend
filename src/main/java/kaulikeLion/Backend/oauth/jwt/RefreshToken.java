5365947026c3e6feb9ea51abf7a32b526fe902b2
persi0815 <jina_8151010@naver.com>
2024-03-04 14:32:12 +0900

    Refactor: redis import 삭제

--- src/main/java/kaulikeLion/Backend/oauth/jwt/RefreshToken.java
+++ src/main/java/kaulikeLion/Backend/oauth/jwt/RefreshToken.java
@@ -5,13 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.Collection;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.TimeToLive;
-import org.springframework.data.redis.core.index.Indexed;
-import org.springframework.security.core.GrantedAuthority;
-
 import java.util.Collection;
 
 @Entity
